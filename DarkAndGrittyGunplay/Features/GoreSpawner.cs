53a34bba1b93964bd279205e2e9af0fdc2d9d4e4
Oscar <70030002+icedchai@users.noreply.github.com>
2026-06-01 19:04:38 -0500

    Update GoreSpawner.cs

--- DarkAndGrittyGunplay/Features/GoreSpawner.cs
+++ DarkAndGrittyGunplay/Features/GoreSpawner.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Mirror;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using LabApi.Features.Console;
+using Logger = LabApi.Features.Console.Logger;
 
 namespace DarkAndGrittyGunplay.Features
 {
@@ -12,6 +15,8 @@ namespace DarkAndGrittyGunplay.Features
         public static GoreSpawner Singleton { get; internal set; }
 
         public Queue<Gib> gibQueue { get; private set; }
+
+        public int ValidDeadPeople { get; internal set; } = 0;
 
 
         private void Start()
@@ -22,14 +27,22 @@ namespace DarkAndGrittyGunplay.Features
 
         private void Update()
         {
+            if (ValidDeadPeople != 0)
+            {
+                ValidDeadPeople = 0;
+            }
             if (!gibQueue.IsEmpty())
             {
                 for (int i = 0; i < Plugin.Singleton.Config.MaxGibsActivatedPerTick; i++)
                 {
-                    Gib gib = gibQueue.Dequeue();
-                    if (gib == null)
+                    if (!gibQueue.TryDequeue(out Gib? gib))
                     {
                         return;
+                    }
+
+                    if (gib == null)
+                    {
+                        continue;
                     }
 
                     gib.Activate();
